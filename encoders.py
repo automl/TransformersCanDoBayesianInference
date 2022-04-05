08ce667136fcac31fc8471a6caceaee352f63ffb
SamuelGabriel <Samuel.g.mueller@web.de>
2022-04-05 18:33:37 +0200

    Update encoders.py

--- encoders.py
+++ encoders.py
@@ -5,77 +5,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
-class _PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.):
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-        self.d_model = d_model
-        self.device_test_tensor = nn.Parameter(torch.tensor(1.))
-
-    def forward(self, x):# T x B x num_features
-        assert self.d_model % x.shape[-1]*2 == 0
-        d_per_feature = self.d_model // x.shape[-1]
-        pe = torch.zeros(*x.shape, d_per_feature, device=self.device_test_tensor.device)
-        #position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        interval_size = 10
-        div_term = (1./interval_size) * 2*math.pi*torch.exp(torch.arange(0, d_per_feature, 2, device=self.device_test_tensor.device).float()*math.log(math.sqrt(2)))
-        #print(div_term/2/math.pi)
-        pe[..., 0::2] = torch.sin(x.unsqueeze(-1) * div_term)
-        pe[..., 1::2] = torch.cos(x.unsqueeze(-1) * div_term)
-        return self.dropout(pe).view(x.shape[0],x.shape[1],self.d_model)
-
-
-class EmbeddingEncoder(nn.Module):
-    def __init__(self, num_features, em_size, num_embs=100):
-        super().__init__()
-        self.num_embs = num_embs
-        self.embeddings = nn.Embedding(num_embs * num_features, em_size, max_norm=True)
-        self.init_weights(.1)
-        self.min_max = (-2,+2)
-
-    @property
-    def width(self):
-        return self.min_max[1] - self.min_max[0]
-
-    def init_weights(self, initrange):
-        self.embeddings.weight.data.uniform_(-initrange, initrange)
-
-    def discretize(self, x):
-        split_size = self.width / self.num_embs
-        return (x - self.min_max[0] // split_size).int().clamp(0, self.num_embs - 1)
-
-    def forward(self, x):  # T x B x num_features
-        x_idxs = self.discretize(x)
-        x_idxs += torch.arange(x.shape[-1], device=x.device).view(1, 1, -1) * self.num_embs
-        # print(x_idxs,self.embeddings.weight.shape)
-        return self.embeddings(x_idxs).mean(-2)
-
 Linear = nn.Linear
-MLP = lambda num_features, emsize: nn.Sequential(nn.Linear(num_features+1,emsize*2),
-                                                 nn.ReLU(),
-                                                 nn.Linear(emsize*2,emsize))
 
-class Conv(nn.Module):
-    def __init__(self, input_size, emsize):
-        super().__init__()
-        self.convs = torch.nn.ModuleList([nn.Conv2d(64 if i else 1, 64, 3) for i in range(5)])
-        self.linear = nn.Linear(64,emsize)
+def get_normalized_uniform_encoder(encoder_creator):
+    """
+    This can be used to wrap an encoder that is fed uniform samples in [0,1] and normalizes these to 0 mean and 1 std.
+    For example, it can be used as `encoder_creator = get_normalized_uniform_encoder(encoders.Linear)`, now this can
+    be initialized with `encoder_creator(feature_dim, in_dim)`.
+    :param encoder:
+    :return:
+    """
+    return lambda in_dim, out_dim: nn.Sequential(Normalize(.5, math.sqrt(1/12)), encoder_creator(in_dim, out_dim))
 
-
-    def forward(self, x):
-        size = math.isqrt(x.shape[-1])
-        assert size*size == x.shape[-1]
-        x = x.reshape(*x.shape[:-1], 1, size, size)
-        for conv in self.convs:
-            if x.shape[-1] < 4:
-                break
-            x = conv(x)
-            x.relu_()
-        x = nn.AdaptiveAvgPool2d((1,1))(x).squeeze(-1).squeeze(-1)
-        return self.linear(x)
-
-
-Positional = lambda _, emsize: _PositionalEncoding(d_model=emsize)
 
 
 class CanEmb(nn.Embedding):
